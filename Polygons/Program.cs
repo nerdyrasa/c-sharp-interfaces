bf3fcc3da45a6e3ee84705ae5bd0498f7b77fc2e
nerdyrasa <nerdyrasa@users.noreply.github.com>
2016-05-12 14:27:30 -0500

    Initial commit

---
+++ Polygons/Program.cs
@@ -0,0 +1,38 @@
+﻿using Polygons.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var square = new Square(5);
+            DisplayPolygon("Square", square);
+
+        }
+
+        public static void DisplayPolygon(string polygonType, dynamic polygon)
+        {
+            try
+            {
+                Console.WriteLine($"{polygonType} Number of Sides: {polygon.NumberOfSides}");
+                Console.WriteLine($"{polygonType} SideLength: {polygon.SideLength}");
+                Console.WriteLine($"{polygonType} Perimeter: {polygon.GetPerimeter()}");
+                Console.WriteLine($"{polygonType} Area: {Math.Round(polygon.GetArea(),2)}");
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception thrown while trying to process {polygonType}: \n{ ex.GetType().Name}");
+            }
+            
+        }
+    }
+
+ 
+}
